a58b7e8ece2e7ecc6f15112763059601465ef194
hossainchisty <hossain.chisty11@gmail.com>
2024-03-21 03:56:25 +0600

    chore: remove mistake commits

--- fitness/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from fitness.models import Exercise, WorkoutPlan, WorkoutPlanExercise
-
-admin.site.register(Exercise)
-admin.site.register(WorkoutPlan)
-admin.site.register(WorkoutPlanExercise)
