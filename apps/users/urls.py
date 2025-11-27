300e322f691de39828bbaa693fe20231eece261d
hossainchisty <hossain.chisty11@gmail.com>
2025-11-27 16:23:03 +0600

    feat: Implement user authentication and management APIs including registration, login, profile, and password change, along with login tests.

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -2,12 +2,13 @@
 User URL Configuration
 """
 from django.urls import path
-from .views import UserRegistrationView, UserProfileView, ChangePasswordView
+from .views import UserRegistrationView, UserProfileView, ChangePasswordView, CustomTokenObtainPairView
 
 app_name = "users"
 
 urlpatterns = [
     path("register/", UserRegistrationView.as_view(), name="register"),
+    path("login/", CustomTokenObtainPairView.as_view(), name="login"),
     path("profile/", UserProfileView.as_view(), name="profile"),
     path("change-password/", ChangePasswordView.as_view(), name="change-password"),
 ]
